87acca0bb5f573d1d4d3242e91d6aa7681b004d6
Lucas Pinheiro <lucaslogos@gmail.com>
2023-10-24 14:41:27 -0300

    update

--- back/src/ProEventos.API/Controllers/EventoController.cs
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
+
+
+
 
 namespace ProEventos.API.Controllers
 {
@@ -20,7 +27,24 @@ namespace ProEventos.API.Controllers
         [HttpGet]
         public string Get()
         {
-            return "value";
+            return "Exemplo get";
         }
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo Post";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo Put = {id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo Delete = {id}";
+        }
+
+
     }
 }
